acb1a97fb0e25628706b0af341ccf1f7cac02088
iorellana <iorellana@users.noreply.github.com>
2013-08-18 19:55:07 +0000

    estructura princpial

---
+++ codigo/ej1/ej1.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
---
+++ codigo/ej3/ej3.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <vector>
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
