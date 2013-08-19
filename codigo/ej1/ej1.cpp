862a422d3b1adcc88163422262bfd3da983bb679
iorellana <iorellana@users.noreply.github.com>
2013-08-19 21:38:45 +0000

    ej1 lector de datos de entrada

--- codigo/ej1/ej1.cpp
+++ codigo/ej1/ej1.cpp
@@ -7,7 +7,36 @@
 
 using namespace std;
 
-int main() {
+typedef int Paquete;
+typedef vector<Paquete> Paquetes;
 
+vector<int> ej1(int limite, int cantPaquetes, Paquetes ps) {
+	vector<int> res;
+	return res;
+}
+
+int main() {
+	char termino = ' ';
+	while (termino != '#') {
+		int limite;
+		int cantPaquetes;
+		cin >> limite;
+		cin >> cantPaquetes;
+
+		Paquetes ps;
+		Paquete p;
+		cin >> p;
+
+		for (int i = 0; i < cantPaquetes; ++i)
+		{
+			cin >> p;
+			ps.push_back(p);
+		}
+		vector<int> res;
+		res = ej1(limite, cantPaquetes, ps);
+		for (int i = 0; i < res.size(); ++i)
+			cout << res[i] << " ";
+		termino = (cin >> ws).peek();
+	}
 	return 0;
 }
