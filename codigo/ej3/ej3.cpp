8c8375bd200b707af3aa10bf2ddda6c88a1fb77f
iorellana <iorellana@users.noreply.github.com>
2013-08-19 22:51:33 +0000

    estructura del 3

--- codigo/ej3/ej3.cpp
+++ codigo/ej3/ej3.cpp
@@ -7,6 +7,19 @@
 
 using namespace std;
 
+struct casilla {
+	int tipo;
+	// 0: pared, 1: piso, 2: importante.
+	int laser;
+	// 0: libre, 1: laser horizontal, 2: laser vertical, 3: ambos.
+	bool ocupado;
+	// Si hay un sensor ahi
+	int tipoSensor;
+	// 1: bidireccional, 2: horizontal, 3: vertical.
+};
+
+typedef vector< vector <casilla> > grilla;
+
 int main() {
 	
 	return 0;
