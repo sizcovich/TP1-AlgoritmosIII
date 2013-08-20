6a6873fb7b282e7129c580e62d3ad05fb4cff7ac
iorellana <iorellana@users.noreply.github.com>
2013-08-20 00:09:45 +0000

    lectura y estructuras para el ej1 y ej3

--- codigo/ej3/ej3.cpp
+++ codigo/ej3/ej3.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-struct casilla {
+struct Casilla {
 	int tipo;
 	// 0: pared, 1: piso, 2: importante.
 	int laser;
@@ -16,11 +16,47 @@ struct casilla {
 	// Si hay un sensor ahi
 	int tipoSensor;
 	// 1: bidireccional, 2: horizontal, 3: vertical.
+	Casilla () {
+		laser = 0;
+		ocupado = false;
+		tipoSensor = -1;
+	}
 };
 
-typedef vector< vector <casilla> > grilla;
+typedef vector< vector <Casilla> > Grilla;
+
+vector< vector< int > > ej3(Grilla g) {
+	vector < vector <int > > res;
+	return res;
+}
 
 int main() {
-	
+	char termino = ' ';
+	while (termino != '#') {
+		int n, m;
+		cin >> n;
+		cin >> m;
+
+		vector<Casilla> filaC;
+		Casilla c;
+		Grilla g;
+
+		for (int i = 0; i < n; ++i) {
+			g.push_back(filaC);
+			for (int j = 0; j < m; ++j) {
+				cin >> c.tipo;
+				g[i].push_back(c);
+			}
+		}
+		vector<vector<int> > res;
+		res = ej3(g);
+		for (int i = 0; i < res.size(); ++i) {
+			for(int j = 0; j < res[i].size(); ++j) {
+				cout << res[i][j] << " ";
+			}
+			cout << endl;
+		}
+		termino = (cin >> ws).peek();
+	}
 	return 0;
 }
