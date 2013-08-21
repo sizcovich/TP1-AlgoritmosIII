e8fff48314d313cc645404c6721fc6bc452f9614
magarcia <magarcia@2a63f8a5-0c70-4908-8230-338959155f58>
2013-08-21 06:40:05 +0000

    codigo y demo del ej1

--- codigo/ej1/ej1.cpp
+++ codigo/ej1/ej1.cpp
@@ -4,14 +4,58 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
+// MAS QUE HEAP
+#include <map>
+
+/* -------------------------------------------------------------
+
+Ejercicio 1
+
+Referencia de la estructura map en http://en.cppreference.com/w/cpp/container/map
+ejemplos en http://www.cplusplus.com/reference/map/map/
+
+El algoritmo esta basado en el pseudocodigo del informe.
+
+---------------------------------------------------------------- */
 
 using namespace std;
 
 typedef int Paquete;
+typedef int Camion; //El camion se define por su capacidad
 typedef vector<Paquete> Paquetes;
+
+Camion camionMenosCargado(map<Camion, int> ca)
+{
+    map<int,int>::iterator it = ca.begin();
+    return (it->first);
+}
 
 vector<int> ej1(int limite, int cantPaquetes, Paquetes ps) {
 	vector<int> res;
+	//estructura
+	map<Camion, int> ca; //El segundo int es al cuete basicamente
+	int cantCamiones = 1;
+
+    for (int i=0;i<cantPaquetes;++i)
+    {
+        Camion c = camionMenosCargado(ca);
+        if (Paquetes[i]<= c)
+        {
+            c -= Paquetes[i];
+            map<int,int>::iterator it = ca.begin();
+            ca.erase(it); //elimina el camion menos cargado y en la siguiente linea lo vuelve a agregar
+            ca.emplace(make_pair(std::int(c), std::int(0))); //agrega el valor c
+        }else{
+            ca.emplace(make_pair(std::int(limite-Paquetes[i]), std::int(0)));
+            ++cantCamiones;
+        }
+    }
+
+    //Guardo los resultados en el vector
+    res.push_back(cantCamiones);
+    for (map<int,int>::iterator it2=mymap.begin(); it2!=mymap.end(); ++it2)
+        res.push_back(it2->first);
+
 	return res;
 }
 
