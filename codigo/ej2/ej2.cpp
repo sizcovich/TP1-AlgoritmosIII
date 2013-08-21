108f142917b5f84c4856eb76a6646602c844251d
magarcia <magarcia@2a63f8a5-0c70-4908-8230-338959155f58>
2013-08-21 16:45:14 +0000

    estructura ej2

--- codigo/ej2/ej2.cpp
+++ codigo/ej2/ej2.cpp
@@ -7,7 +7,36 @@
 
 using namespace std;
 
+typedef pair<int> Curso;
+
+
+vector<int> ej2(int cantCursos, vector<Curso> cs) {
+	vector<int> res;
+	return res;
+}
+
 int main() {
-    
+    	char termino = ' ';
+	while (termino != '#') {
+		int c,ci,cf;
+		int cantCursos;
+		vector<Curso> cs;
+
+		cin >> cantCursos;
+
+		for (int i = 0; i < cantCursos; ++i)
+		{
+			cin >> ci;
+			cin >> cf;
+			cs.push_back(pair<int,int>(int(ci), int(cf)));
+		}
+
+		vector<int> res;
+		res = ej2(cantCursos,cs);
+		for (int i = 0; i < res.size(); ++i)
+			cout << res[i] << " ";
+        cout<<endl;
+		termino = (cin >> ws).peek();
+	}
 	return 0;
 }
