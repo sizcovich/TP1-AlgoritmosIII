66096834084f06e8007921800f336c212d482ca0
Sabrina <sizcovich@users.noreply.github.com>
2013-08-22 03:00:38 +0000

    no compila asi que lo tengo que arreglar pero estaria asi

--- codigo/ej1/ej1.cpp
+++ codigo/ej1/ej1.cpp
@@ -24,10 +24,10 @@ typedef pair<int,int> Camion; //El camion se define por su capacidad
 typedef vector<Paquete> Paquetes;
 
 
-vector<int> algoritmoDePascual(int limite, int cantPaquetes, Paquetes ps) {
+pair<int,vector<int> > algoritmoDePascual(int limite, int cantPaquetes, Paquetes ps) {
 	vector<int> vectorCamionesOrdenados[cantPaquetes];
-	pair<int,vector<int>> res;
-	priority_queue<Camion, int> ca; //el entero representa el orden del camion
+	pair<int,vector<int> > res;
+	priority_queue<pair<Camion, int> > ca; //el entero representa el orden del camion
 	int cantCamiones = 0; //contador de camiones
 
     for (int i=0;i<cantPaquetes;++i)
@@ -35,20 +35,22 @@ vector<int> algoritmoDePascual(int limite, int cantPaquetes, Paquetes ps) {
         Camion c = ca.top();
         if (ps[i]+c <= limite)
         {
-            c.second() += ps[i];
+            c.second += ps[i];
             ca.pop(); //elimina el camion menos cargado y en la siguiente linea lo vuelve a agregar
             ca.push(c);
         }else{
-            ca.push(make_pair(ps[i], cantCamiones); //agrega un nuevo camion
+            ca.push(make_pair(ps[i], cantCamiones)); //agrega un nuevo camion
             ++cantCamiones;
         }
     }
     //Guardo los resultados en el vector         
 	while(!ca.empty()){
 		Camion c = ca.top(); ca.pop();
-		vectorCamionesOrdenados[c.first()] = c;
+		vectorCamionesOrdenados[c.first] = c;
 	}
 	vectorCamionesOrdenados.resize(cantCamiones);
+    res.first = cantCamiones + 1; //como empieza en 0 le sumo 1
+    res.second = vectorCamionesOrdenados;
 	return res; //devuelvo la tupla (cantCamiones,[c1 c2 ... cn])
 }
 
