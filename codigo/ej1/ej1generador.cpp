3bf441369c82130d45f9fccd94235d0c87031963
sebastianvita <sevita@users.noreply.github.com>
2013-09-03 20:25:47 +0000

    ej1.dat para q sabri grafique

---
+++ codigo/ej1/ej1generador.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <functional>
+
+using namespace std;
+
+
+int main() {
+	srand(time(NULL));
+	for (int i = 5000; i <= 100000 ; i=i+5000)
+	{
+		cout << 120 << " " << i << " " ;
+		for (int j = 0; j < i; j++)
+		{
+			cout << rand() % 100 << " ";
+		}
+		cout << endl;
+	}
+	cout << "#";
+	return 0;
+}
