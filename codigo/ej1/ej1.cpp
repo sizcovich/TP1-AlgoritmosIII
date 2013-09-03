185776bc9c07719a6b7b6fca29c9fc93f7dd293a
iorellana <iorellana@users.noreply.github.com>
2013-09-03 23:10:56 +0000

    ej2.tex

--- codigo/ej1/ej1.cpp
+++ codigo/ej1/ej1.cpp
@@ -93,9 +93,9 @@ int main() {
 		pair<int,vector<int> > res;
 
 		auto t1 = chrono::high_resolution_clock::now();
-		for (int i = 0; i < 10; ++i)
-		{
-		res = algoritmoDePascual(limite, cantPaquetes, ps);
+		for (int i = 0; i < 10; ++i) {
+			ps.clear();
+			res = algoritmoDePascual(limite, cantPaquetes, ps);
 		}
 		auto t2 = chrono::high_resolution_clock::now();
 		auto x = chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count();
