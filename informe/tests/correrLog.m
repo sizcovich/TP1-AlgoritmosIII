8479a655824fa83af6e5c320f4f1ea8646e81878
Sabrina <sizcovich@users.noreply.github.com>
2013-09-06 06:28:36 +0000

    grapj

--- informe/tests/correrLog.m
+++ informe/tests/correrLog.m
@@ -1,9 +1,13 @@
 function [res] = correrLog(input)
-n = size(input);
-res = zeros(size(input));
-for i=1:n
-    m = i*log(i);
-    res(i) = m;
+res = zeros(100,1);
+i = 1000;
+n = 1;
+while i <= input
+    m = log10(i);
+    m = m * i;
+    res(n,1) = m;
+    i = i + 1000;
+    n = n+1;
 end
 end
 
